6c1421213a21fc7e5f5b9062b09bcbc003c8c296
Yoan Roullard <yoan.roullard@gmail.com>
2015-05-27 19:08:31 +0200

    [Draft/PoC] Compile lilypond data to pdf

---
+++ js/lilypond.js
@@ -0,0 +1,44 @@
+var shell = require('shelljs/global');
+
+
+/**
+ * Add title and arranger's name to our lilypond file
+ * @param {String} music  lilypond music code
+ * @param {Object} metada sheet's metadata
+ * FIXME : To move somewhere else (maybe on a module called "utils" ?)
+ */
+var _appendTitleAndArranger = function(music,metada){
+  var template = '\\header{ title = "${title}" composer="${arranger}" }';
+
+  template = template.replace('${title}',metadata.title);
+  template = template.replace('${arranger}',metadata.arranger);
+  music += template;
+  return music;
+}
+
+
+
+
+/**
+ * Compile our lilypond data to pdf
+ * @param  {String} music    lilypond data
+ * @param  {Object} metadata sheet metadata
+ *
+ */
+var compile = function(music,metadata){
+  try{
+    var title = metadata.title || 'output';
+    var musicToCompile = _appendTitleAndArranger(music,metadata);
+    musicToCompile.to('./.tmp/'+title+'.ly');
+    exec('lilypond -o ./output/'+title+' ./.tmp/'+title+'.ly');
+  }
+  catch(e){
+    console.error(e);
+  }
+}
+
+
+
+module.exports = {
+  compile : compile
+}
